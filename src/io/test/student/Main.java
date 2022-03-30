b92615e437fac48094e5475b2535f08079356dca
AnDr <avaks.ua@google.com>
2022-03-30 21:28:40 +0300

    main 1

--- src/io/test/student/Main.java
+++ src/io/test/student/Main.java
@@ -1,4 +1,19 @@
 package io.test.student;
 
+import java.util.Date;
+
 public class Main {
+    @Deprecated
+    public static void main(String[] args){
+        final StudentList sl = new StudentList();
+
+        sl.add(new Student("Anton","Serebin",new Date(1995,11,1)));
+        sl.add(new Student("Petr","Kupeloff",new Date(1974,07,25)));
+        sl.add(new Student("Alex","Levin",new Date(1989,05,05)));
+
+        int n = sl.find("Alex");
+        System.out.println(sl.get(n).getName());
+        System.out.println(sl.get(n).getSurname());
+        System.out.println(sl.get(n).getBirth().toString());
+    }
 }
