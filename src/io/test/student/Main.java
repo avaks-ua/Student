ec6b2d536eb9254dcd4e91bb115d80f9b00ded6c
AnDr <avaks.ua@google.com>
2022-03-30 23:49:41 +0300

    main 1

--- src/io/test/student/Main.java
+++ src/io/test/student/Main.java
@@ -7,13 +7,44 @@ public class Main {
     public static void main(String[] args){
         final StudentList sl = new StudentList();
 
-        sl.add(new Student("Anton","Serebin",new Date(1995,11,1)));
-        sl.add(new Student("Petr","Kupeloff",new Date(1974,07,25)));
-        sl.add(new Student("Alex","Levin",new Date(1989,05,05)));
+        sl.add(new Student("Anton","Serebin",new Date(95,11,1)));
+        sl.add(new Student("Petr","Kupeloff",new Date(74,07,25)));
+        sl.add(new Student("Alex","Levin",new Date(89,05,05)));
 
-        int n = sl.find("Alex");
-        System.out.println(sl.get(n).getName());
-        System.out.println(sl.get(n).getSurname());
-        System.out.println(sl.get(n).getBirth().toString());
+        int i, n;
+        for (i=0;i<2;i++) {
+
+            if(i==1)
+                n = sl.find("Alex");
+            else n = sl.findsuname("Kupeloff");
+            System.out.println(sl.get(n).getName());
+            System.out.println(sl.get(n).getSurname());
+            System.out.println(sl.get(n).getBirth().toString());
+            System.out.println("-----------------");
+        }
+            printArray(sl);
+//        int ii;
+//        for (ii=0; ii < sl.p ; ii++) {
+//            System.out.print(sl.get(ii).getName()+ " ") ;
+//            System.out.print(sl.get(ii).getSurname()+ " ");
+//            System.out.print(sl.get(ii).getBirth().toString()+ " ");
+//            System.out.println("");
+//        }
+       if (sl.delstud("Levin")){
+            System.out.println("----Ok---");
+            printArray(sl);
+        }
+
     }
+
+    static void printArray(StudentList sl){
+        int ii;
+        for (ii=0; ii <  sl.p ; ii++) {
+            System.out.print(sl.get(ii).getName()+ " ") ;
+            System.out.print(sl.get(ii).getSurname()+ " ");
+            System.out.print(sl.get(ii).getBirth().toString()+ " ");
+            System.out.println("");
+        }
+    }
+
 }
--- src/io/test/student/StudentList.java
+++ src/io/test/student/StudentList.java
@@ -3,7 +3,7 @@ package io.test.student;
 public class StudentList {
 
     private final Student[] list = new Student[100];
-    private int p = 0;
+    int p = 0;
 
     public void add(Student s){
         list[p++] = s;
@@ -20,4 +20,32 @@ public class StudentList {
         }
         return -1;
     }
+
+    public int findsuname(String suname){
+        for(int i=0; i<p; i++){
+            if (list[i].getSurname().equalsIgnoreCase(suname))
+                return  i;
+        }
+        return -1;
+    }
+
+    public boolean delstud(String suname){
+         for(int i=0; i<p; i++){
+            if (list[i].getSurname().equalsIgnoreCase(suname)) {
+                for (int index = i; index < list.length - 1; index++) {
+                    if (!(this.list[index + 1] == null))
+                        list[index] = list[index + 1];
+                    else {
+                        this.list[index] = null;
+                        break;
+                    }
+                }
+                p = p - 1;
+                return true;
+            }
+            }
+
+         return false;
+    }
+
 }
