f5965844d6806ce5a49af9547cfcfafc2367be4c
AnDr <avaks.ua@google.com>
2022-03-30 21:09:36 +0300

    studlist 1

--- src/io/test/student/StudentList.java
+++ src/io/test/student/StudentList.java
@@ -2,5 +2,22 @@ package io.test.student;
 
 public class StudentList {
 
-    private Student[] list = new Student[100];
+    private final Student[] list = new Student[100];
+    private int p = 0;
+
+    public void add(Student s){
+        list[p++] = s;
+    }
+
+    public Student get(int n){
+        return list[n];
+    }
+
+    public int find(String name){
+        for(int i=0; i<p; i++){
+            if (list[i].getName().equalsIgnoreCase(name))
+                return  i;
+        }
+        return -1;
+    }
 }
